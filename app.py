9323a8527c702bf75af7c7a0eef2945568e87ef6
eunsssang12 <qead6090@gmail.com>
2024-07-13 21:42:26 +0900

    01 개발환경세팅

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+# 웹 서버 역할 Flask App
+app = Flask(__name__)  # 매직 변수
+
+# 라우터 설정
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/page')
+def page():
+    return render_template("page.html")
+
+
+# Flask App 가동 (run)
+if __name__ == "__main__":
+    app.run(debug=True)   
+    
+# 루프백,  i, http port 검색해보기
+# http - 80
+# https - 443
+# blueprint 공부
+# CRUD
