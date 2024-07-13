e037648c79dcb5ec6202e814723d5b419c6ab893
eunsssang12 <qead6090@gmail.com>
2024-07-13 23:59:55 +0900

    Add new authentication feature and update diary module

--- diary/__init__.py
+++ diary/__init__.py
@@ -5,4 +5,11 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET KEY'] = 'sadfafdjsdlfjljslfjljl'
     
+    # Import Blueprint
+    from .views import views
+    from .auth import auth
+    
+    # Apply BluePrint
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/')
     return app
