7cb860c91c5fa43017c112ec14bb2ead7a966857
eunsssang12 <150026397+eunsssang12@users.noreply.github.com>
2024-07-13 23:46:14 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,28 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
 # 웹 서버 역할 Flask App
-app = Flask(__name__)  # 매직 변수
+# app = Flask(__name__)  # 매직 변수
 
 # 라우터 설정
-@app.route('/')
-def index():
-    return render_template("index.html")
+# @app.route('/')
+# def index():
+#     return render_template("index.html")
 
-@app.route('/page')
-def page():
-    return render_template("page.html")
+# @app.route('/page')
+# def page():
+#     return render_template("page.html")
 
 
 # Flask App 가동 (run)
-if __name__ == "__main__":
-    app.run(debug=True)   
+# if __name__ == "__main__":
+#     app.run(debug=True)   
      
 # 루프백,  i, http port 검색해보기
 # http - 80
 # https - 443
 # blueprint 공부
 # CRUD (Create, Read, Update, Delete)
+# __name__ 공부하기
+# globals() 함수
+# python decorator 문법
+
